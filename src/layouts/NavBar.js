d5e98866c87dd277f325205f781965e8116aa064
bizip <bizip04@gmail.com>
2022-06-28 12:52:25 +0200

    Fix tthe text

--- src/layouts/NavBar.js
+++ src/layouts/NavBar.js
@@ -6,9 +6,30 @@ const NavBar = () => (
   <nav>
     <h1>Math magicians</h1>
     <ul className="nav-links">
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/Calculator">Calculator</NavLink></li>
-      <li><NavLink to="/quotes">Quotes</NavLink></li>
+      <li>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? 'active' : 'deactive')}
+        >
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/Calculator"
+          className={({ isActive }) => (isActive ? 'active' : 'deactive')}
+        >
+          Calculator
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/quotes"
+          className={({ isActive }) => (isActive ? 'active' : 'deactive')}
+        >
+          Quotes
+        </NavLink>
+      </li>
     </ul>
   </nav>
 );
